91ec4b4c82708904f6b02f9218733950774d0add
Ap2ach <mdigtk@naver.com>
2018-12-27 18:56:11 +0900

    Exam1.java 수정 - 간단한 깃 명령어 추가

--- src/exam/Exam1.java
+++ src/exam/Exam1.java
@@ -7,6 +7,10 @@ public class Exam1 {
 		int x=2;
 		int y=44;
 		System.out.println(x+y);
+		//clone : 원격 저장소 복사 
+		//add: 스테이지 영역에 작업 파일 추가
+		//commit: 세이브, 스테이지 영역의 파일들을 가지고 커밋(세이브)를 만든다
+		//push: 원격 저장소에 커밋을 업로드 한다. 
 	}
 
 }
