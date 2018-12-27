c0211e8caf3059e7cb8b60cd53f53839d9f962f3
Ap2ach <mdigtk@naver.com>
2018-12-27 19:41:36 +0900

    마스터브랜치 MasterClass.java 생성 Exam1.java checkout 명령어 설명 추가

---
+++ src/exam/MasterClass.java
@@ -0,0 +1,10 @@
+package exam;
+
+public class MasterClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이건 마스터 브랜치에서 만든 자바 파일입니다. ");
+	}
+
+}
