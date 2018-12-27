f1aea7b81b44c839e5447e247a9f8105810535bf
Ap2ach <mdigtk@naver.com>
2018-12-27 18:54:52 +0900

    Exam1.java 생성 Hello.java 간단한 깃 명령어 추가

---
+++ src/exam/Exam1.java
@@ -0,0 +1,12 @@
+package exam;
+
+public class Exam1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=2;
+		int y=44;
+		System.out.println(x+y);
+	}
+
+}
