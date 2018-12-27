01507589394d363e0a000649448d1af8c29e1d64
Ap2ach <mdigtk@naver.com>
2018-12-27 19:09:07 +0900

    Hello1.java 텍스트 추가

--- src/exam/Hello1.java
+++ src/exam/Hello1.java
@@ -1,5 +1,9 @@
 package exam;
 
 public class Hello1 {
-
+	public void main(String[] args) {
+		System.out.println("스누피? ");
+		System.out.println("아직 할줄 몰라서 뻥뻥 터지네요 ");
+		System.out.println("취업하자");
+	}
 }
